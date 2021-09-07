2add3b03c124a0c1be6746739b78bce977959665
Zhang Jie <838216870@qq.com>
2021-09-07 21:50:08 +0800

    feat:类完善

--- class/index.js
+++ class/index.js
@@ -1,33 +1,32 @@
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  
-  Person.prototype.say = function () {
-    console.log("我叫" + this.name + "今年" + this.age + "岁");
-  };
-  
-  var p1 = new Person("张三", 18);
-  var p2 = new Person("李四", 20);
-  
-  p1.say();
-  p2.say();
-  
-  // 能做什么呢？？
-  
-  function Button(options) {
-    this.buttonElement = document.createElement("button");
-    this.buttonElement.innerText = options.value;
-    this.buttonElement.style.backgroundColor = options.backgroundColor;
-    this.buttonElement.onclick = options.onClick;
-    document.body.appendChild(this.buttonElement);
-  }
-  
-  var b1 = new Button({
-    value: "按钮",
-    backgroundColor: "aqua",
-    onClick: function () {
-      alert("Hello");
-    },
-  });
-  
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.say = function () {
+  console.log("我叫" + this.name + "今年" + this.age + "岁");
+};
+
+var p1 = new Person("张三", 18);
+var p2 = new Person("李四", 20);
+
+p1.say();
+p2.say();
+
+// 能做什么呢？？
+
+function Button(options) {
+  this.buttonElement = document.createElement("button");
+  this.buttonElement.innerText = options.value;
+  this.buttonElement.style.backgroundColor = options.backgroundColor;
+  this.buttonElement.onclick = options.onClick;
+  document.body.appendChild(this.buttonElement);
+}
+
+var b1 = new Button({
+  value: "按钮",
+  backgroundColor: "aqua",
+  onClick: function () {
+    alert("Hello");
+  },
+});
