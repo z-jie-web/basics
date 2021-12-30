3cf89d3244d8892a3d59f79338f3335e47090c1e
zhangjie <wb-zj936129@alibaba-inc.com>
2021-12-30 09:47:05 +0800

    feat:本地修改

--- class/index.js
+++ class/index.js
@@ -1,33 +1,38 @@
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
   
-  Person.prototype.say = function () {
-    console.log("我叫" + this.name + "今年" + this.age + "岁");
-  };
+//   Person.prototype.say = function () {
+//     console.log("我叫" + this.name + "今年" + this.age + "岁");
+//   };
   
-  var p1 = new Person("张三", 18);
-  var p2 = new Person("李四", 20);
+//   var p1 = new Person("张三", 18);
+//   var p2 = new Person("李四", 20);
   
-  p1.say();
-  p2.say();
+//   p1.say();
+//   p2.say();
   
-  // 能做什么呢？？
+//   // 能做什么呢？？
   
-  function Button(options) {
-    this.buttonElement = document.createElement("button");
-    this.buttonElement.innerText = options.value;
-    this.buttonElement.style.backgroundColor = options.backgroundColor;
-    this.buttonElement.onclick = options.onClick;
-    document.body.appendChild(this.buttonElement);
-  }
+//   function Button(options) {
+//     this.buttonElement = document.createElement("button");
+//     this.buttonElement.innerText = options.value;
+//     this.buttonElement.style.backgroundColor = options.backgroundColor;
+//     this.buttonElement.onclick = options.onClick;
+//     document.body.appendChild(this.buttonElement);
+//   }
   
-  var b1 = new Button({
-    value: "按钮",
-    backgroundColor: "aqua",
-    onClick: function () {
-      alert("Hello");
-    },
-  });
+//   var b1 = new Button({
+//     value: "按钮",
+//     backgroundColor: "aqua",
+//     onClick: function () {
+//       alert("Hello");
+//     },
+//   });
   
+
+var re = new RegExp("^(http|https)://","i");
+var str =undefined
+var match = re.test(str);
+console.log(match)
